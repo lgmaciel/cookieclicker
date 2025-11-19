ddbbf1c6b2d649ab0acfafb3327df99226f5bb69
lgmaciel <132719840+lgmaciel@users.noreply.github.com>
2025-11-19 16:47:40 -0300

    rascunho das construções

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ const imgCookie = document.querySelector("#img-cookie");
 let valorQuantCookies = 0
 quantCookies.value = valorQuantCookies;
 
-imgCookie.addEventListener("click", atualizarQuantCookies)
+imgCookie.addEventListener("click", atualizarJogo)
 
 function atualizarQuantCookies() {
     //atualizando valor do placar
     valorQuantCookies = valorQuantCookies + 1
     //mostrando novo valor do placar
     quantCookies.value = valorQuantCookies
+}
+
+function atualizarJogo() {
+
+    atualizarQuantCookies()
+
+    // Regras do jogo
+
+    if(valorQuantCookies >= 10) {
+        const imgCursor = document.querySelector("#img-cursor");
+        imgCursor.style.borderColor = "red";
+    }
+
+
+    if(valorQuantCookies >= 50) {
+        const imgCursor = document.querySelector("#img-vovo");
+        imgCursor.style.borderColor = "red";
+    }
+
 }
